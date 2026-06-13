d949c2707b6e6f3eda0b4ce37cfaf889ac7ff357
wauputr4 <103489788+wauputr4@users.noreply.github.com>
2026-06-14 01:23:32 +0700

    Add mobile app nav and SEO metadata

---
+++ src/pages/sitemap-index.xml.ts
@@ -0,0 +1,26 @@
+const pages = [
+  '/',
+  '/falsafah/',
+  '/projects/',
+  '/blog/',
+  '/blog/memperkenalkan-blog-indopensource/',
+  '/forum/',
+  '/contact/'
+];
+
+export function GET() {
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages
+  .map((page) => `  <url>
+    <loc>${new URL(page, 'https://indopensource.org').toString()}</loc>
+  </url>`)
+  .join('\n')}
+</urlset>`;
+
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8'
+    }
+  });
+}
